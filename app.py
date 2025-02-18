21708bf1aedcdae7a9d1aa365acea6988b9df952
emanuelly <159542297+imnotmanuu@users.noreply.github.com>
2025-02-18 08:56:46 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+print('Sabor Express\n')
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair \n')
+
+opcao_escolhida = int(input('escolha uma opção:'))
+print(f'voce escolheu a opção {opcao_escolhida}')
+
+def finalizar_app()
